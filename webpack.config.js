b95cabe74b93758235883f955e61c7e69c4335d3
Casper M. H. Holmgreen <cholmgreen@gmail.com>
2016-09-18 17:19:40 -0700

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: __dirname + '/src/index.jsx',
+  output: {
+    path: __dirname + '/dist/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+}
